3d760e2bc5823c3d08d5c3cfd1c99882da15efb8
Junniebug <87003782+Junniebug@users.noreply.github.com>
2021-07-31 23:58:37 +0900

    Update imposter.lua

--- imposter.lua
+++ imposter.lua
@@ -1,9 +1,19 @@
+game.Loaded:Wait()
 
+if game['PlaceId'] == 3978370137 then  -- gpo
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
+elseif game['PlaceId'] == 6360478118 then -- gpo universe hub
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
+elseif game['PlaceId'] == 1730877806 then -- gpo main hub
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
+elseif game['PlaceId'] == 2809202155 then -- yba
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
+elseif game['PlaceId'] == 5094651510 then -- demonfall
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
+end
 
 spawn(function()
 if game['PlaceId'] == 2809202155 then return end
-
-game.Loaded:Wait()
 
 local lp = game:GetService('Players').LocalPlayer
 
@@ -29,18 +39,6 @@ sus()
 end)
 
 spawn(function()
-if game['PlaceId'] == 3978370137 then  -- gpo
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
-elseif game['PlaceId'] == 6360478118 then -- gpo universe hub
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
-elseif game['PlaceId'] == 1730877806 then -- gpo main hub
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
-elseif game['PlaceId'] == 2809202155 then -- yba
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
-elseif game['PlaceId'] == 5094651510 then -- demonfall
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
-end
-        game.Loaded:Wait()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/babft.lua'))()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/demonfall.lua'))()
 end)
