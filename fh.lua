f88f27b19061a04744a27e12007b91bd128a655c
Junniebug <87003782+Junniebug@users.noreply.github.com>
2021-08-01 02:39:13 +0900

    Update fh.lua

--- fh.lua
+++ fh.lua
@@ -1,9 +1,4 @@
-if getgenv().amongusxD == true then
-    return
-end
-
 pcall(function()
-      getgenv().amongusxD = true
    getgenv().Key = "June1010";
    getgenv().Id = "682297558353051687";if syn and not(SENTINEL_V2) then r=syn.request else r=request end;
    gets=function() return r({Method="GET",Url="https://observant-caramel-grip.glitch.me/getscript?d=efu7R4kDdcgI5W8kUZ1y&k="..getgenv().Key.."&id="..getgenv().Id.."&d2=FrostHub"}).Body end;rt=0;repeat spawn(function() pcall(function() loadstring(gets())() end) end);rt=rt+1;wait(60) until getgenv().executed or rt >= 10
