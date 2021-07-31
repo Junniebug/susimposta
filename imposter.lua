262fac9298efa3751cf7e0e9028fe872f8a3e02e
Junniebug <87003782+Junniebug@users.noreply.github.com>
2021-08-01 00:57:13 +0900

    Update imposter.lua

--- imposter.lua
+++ imposter.lua
@@ -9,69 +9,68 @@ local sheeesh = {
 5094651510 -- demonfall
 }
 
-for _,xD in pairs(sheeesh) do
-	if game['PlaceId'] == xD then
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
-end
+if game['PlaceId'] == xD then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
 
 spawn(function()
-local lp = game:GetService('Players').LocalPlayer
+    game.Loaded:Wait()
+    local lp = game:GetService('Players').LocalPlayer
 
-if lp.Name == "sjlyant" or "Juneuari" then
+    if lp.Name == "sjlyant" or "Juneuari" then
 
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local Destroy = Instance.new("TextButton")
+    local ScreenGui = Instance.new("ScreenGui")
+    local Frame = Instance.new("Frame")
+    local Destroy = Instance.new("TextButton")
 
-ScreenGui.Name = "ScreenGui"
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-ScreenGui.ResetOnSpawn = false
+    ScreenGui.Name = "ScreenGui"
+    ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    ScreenGui.ResetOnSpawn = false
 
-Frame.Parent = ScreenGui
-Frame.Active = true
-Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Frame.Position = UDim2.new(0.157894731, 0, 0.321041226, 0)
-Frame.Selectable = true
-Frame.Size = UDim2.new(0, 100, 0, 60)
-Frame.Draggable = true
+    Frame.Parent = ScreenGui
+    Frame.Active = true
+    Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Frame.Position = UDim2.new(0.157894731, 0, 0.321041226, 0)
+    Frame.Selectable = true
+    Frame.Size = UDim2.new(0, 100, 0, 60)
+    Frame.Draggable = true
 
-Destroy.Name = "Destroy"
-Destroy.Parent = Frame
-Destroy.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Destroy.Position = UDim2.new(0.0928495973, 0, 0.493851483, 0)
-Destroy.Size = UDim2.new(0, 80, 0, 20)
-Destroy.Font = Enum.Font.SourceSans
-Destroy.Text = "Turn on IY"
-Destroy.TextColor3 = Color3.fromRGB(0, 0, 0)
-Destroy.TextScaled = true
-Destroy.TextSize = 14.000
-Destroy.TextWrapped = true
-Destroy.MouseButton1Click:connect(function()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/sussybaka.lua'))()
-execCmd('hideiy')
-execCmd('antiafk')
-ScreenGui:Destroy()
-end)
-else
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/sussybaka.lua'))()
-execCmd('hideiy')
-execCmd('antiafk')
-end
-            
-getgenv().ws = false
-            
-local function sus()
-    if ws == true then
- -- loadstring(game:HttpGet("https://pastebin.com/raw/UwFCVrhS", true))() someone else's pastebin
-    loadstring(game:HttpGet("https://pastebin.com/raw/14V388Az", true))() -- mine :D
-game.Players.LocalPlayer.Character.Humanoid:AddPropertyEmulator("WalkSpeed")
+    Destroy.Name = "Destroy"
+    Destroy.Parent = Frame
+    Destroy.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    Destroy.Position = UDim2.new(0.0928495973, 0, 0.493851483, 0)
+    Destroy.Size = UDim2.new(0, 80, 0, 20)
+    Destroy.Font = Enum.Font.SourceSans
+    Destroy.Text = "Turn on IY"
+    Destroy.TextColor3 = Color3.fromRGB(0, 0, 0)
+    Destroy.TextScaled = true
+    Destroy.TextSize = 14.000
+    Destroy.TextWrapped = true
+    Destroy.MouseButton1Click:connect(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/sussybaka.lua'))()
+    execCmd('hideiy')
+    execCmd('antiafk')
+    ScreenGui:Destroy()
+    end)
+    else
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/sussybaka.lua'))()
+    execCmd('hideiy')
+    execCmd('antiafk')
     end
-end
+                
+    getgenv().ws = false
+                
+    local function sus()
+        if ws == true then
+    -- loadstring(game:HttpGet("https://pastebin.com/raw/UwFCVrhS", true))() someone else's pastebin
+        loadstring(game:HttpGet("https://pastebin.com/raw/14V388Az", true))() -- mine :D
+    game.Players.LocalPlayer.Character.Humanoid:AddPropertyEmulator("WalkSpeed")
+        end
+    end
 
-game.Players.LocalPlayer.CharacterAdded:Connect(sus)
-sus()
+    game.Players.LocalPlayer.CharacterAdded:Connect(sus)
+    sus()
 end)
 
     
