8bf3be93238a6c10d76ccc886fa73a1a1494e7fd
Junniebug <87003782+Junniebug@users.noreply.github.com>
2021-08-01 02:41:44 +0900

    Update fh.lua

--- fh.lua
+++ fh.lua
@@ -1,5 +1,17 @@
-pcall(function()
+local sheeesh = {
+3978370137, -- gpo
+6360478118, -- gpo universe hub
+1730877806, -- gpo main menu
+2809202155, -- yba
+5094651510 -- demonfall
+}
+
+for _,xD in pairs(sheeesh) do
+	if game['PlaceId'] == xD then
+	pcall(function()
    getgenv().Key = "June1010";
    getgenv().Id = "682297558353051687";if syn and not(SENTINEL_V2) then r=syn.request else r=request end;
    gets=function() return r({Method="GET",Url="https://observant-caramel-grip.glitch.me/getscript?d=efu7R4kDdcgI5W8kUZ1y&k="..getgenv().Key.."&id="..getgenv().Id.."&d2=FrostHub"}).Body end;rt=0;repeat spawn(function() pcall(function() loadstring(gets())() end) end);rt=rt+1;wait(60) until getgenv().executed or rt >= 10
-end)
+      end)
+   end
+end
