c344a4ac1e50d0419f078a97718050cf5212cbdd
Junniebug <87003782+Junniebug@users.noreply.github.com>
2021-08-02 06:52:46 +0900

    Update fh.lua

--- fh.lua
+++ fh.lua
@@ -6,12 +6,21 @@ local sheeesh = {
 5094651510 -- demonfall
 }
 
+local gpo = {
+3978370137, -- gpo
+6360478118, -- gpo universe hub
+1730877806 -- gpo main menu
+}
+
 for _,xD in pairs(sheeesh) do
 	if game['PlaceId'] == xD then
+		if game['PlaceId'] == gpo then game.Loaded:Wait()
+			if game.Players.LocalPlayer.Name == "junniekangbug" then return end
 	pcall(function()
    getgenv().Key = "June1010";
    getgenv().Id = "682297558353051687";if syn and not(SENTINEL_V2) then r=syn.request else r=request end;
    gets=function() return r({Method="GET",Url="https://observant-caramel-grip.glitch.me/getscript?d=efu7R4kDdcgI5W8kUZ1y&k="..getgenv().Key.."&id="..getgenv().Id.."&d2=FrostHub"}).Body end;rt=0;repeat spawn(function() pcall(function() loadstring(gets())() end) end);rt=rt+1;wait(60) until getgenv().executed or rt >= 10
-      end)
+         end)
+      end
    end
 end
