d339e6fca37f1d84a5cc7a316a3d90b9aa034ef7
Junniebug <87003782+Junniebug@users.noreply.github.com>
2021-08-02 22:22:29 +0900

    Update autoexec.lua

--- autoexec.lua
+++ autoexec.lua
@@ -142,7 +142,7 @@ end
 				
 function Repeat()
 if getgenv().poop == true then
-	if game.Players.LocalPlayer.Character ~= nil then
+	repeat wait() until game.Players.LocalPlayer.Character ~= nil
 	local Stage = workspace.BoatStages.NormalStages
         Tween(0.5,game:GetService("Workspace").BoatStages.NormalStages.CaveStage1.DarknessPart.CFrame - Vector3.new(0,0,50))
 	for i = 1,10 do
@@ -151,7 +151,6 @@ if getgenv().poop == true then
 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Stage.TheEnd.GoldenChest.Trigger.CFrame
 	wait(5)
 	game.Players.LocalPlayer.Character.HumanoidRootPart:Destroy()
-	else repeat wait() until game.Players.LocalPlayer.Character ~= nil 
 	end
     end
 end
