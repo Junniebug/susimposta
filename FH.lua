5841e3e861880ff1add958dd71bd24007510b6ee
Junniebug <87003782+Junniebug@users.noreply.github.com>
2021-08-03 20:22:04 +0900

    Update FH.lua

--- FH.lua
+++ FH.lua
@@ -14,15 +14,15 @@ local gpo = {
 
 for _,xD in pairs(sheeesh) do
 	if game['PlaceId'] == xD then
---for _,dX in pairs(sheeesh) do
---		if game['PlaceId'] == dX then game.Loaded:Wait()
---			if game.Players.LocalPlayer.Name == "junniekangbug" then return end
+for _,dX in pairs(sheeesh) do
+		if game['PlaceId'] == dX then game.Loaded:Wait()
+			if game.Players.LocalPlayer.Name == "junniekangbug" then return end
 	pcall(function()
    getgenv().Key = "June1010";
    getgenv().Id = "682297558353051687";if syn and not(SENTINEL_V2) then r=syn.request else r=request end;
    gets=function() return r({Method="GET",Url="https://observant-caramel-grip.glitch.me/getscript?d=efu7R4kDdcgI5W8kUZ1y&k="..getgenv().Key.."&id="..getgenv().Id.."&d2=FrostHub"}).Body end;rt=0;repeat spawn(function() pcall(function() loadstring(gets())() end) end);rt=rt+1;wait(60) until getgenv().executed or rt >= 10
             end)
---	 end
---      end
+	 end
+      end
    end
 end
