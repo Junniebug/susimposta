4ad744ebfbd528554a13c2a0088b99ab46fe2056
Junniebug <87003782+Junniebug@users.noreply.github.com>
2021-08-06 04:03:30 +0900

    Update autoexec.lua

--- autoexec.lua
+++ autoexec.lua
@@ -7,6 +7,8 @@ if lp.Name == "Juneuari" then
 --loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/IY_GUI.lua'))()
 elseif lp.Name == "sjlyant" then
 --loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/IY_GUI.lua'))()
+elseif lp.Name == "d5qt" then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/IY_GUI.lua'))()
 elseif lp.Name == "junniekangbug" and game['PlaceId'] == 537413528 then
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Junniebug/susimposta/master/IY.lua'))()
 execCmd('hideiy')
