e171b069c9fa301e3774fc5c130619bfd53b8650
Junniebug <87003782+Junniebug@users.noreply.github.com>
2021-12-14 21:49:24 +0900

    Update Tower_Defense_Simulator_Autofarm.lua

--- Tower_Defense_Simulator_Autofarm.lua
+++ Tower_Defense_Simulator_Autofarm.lua
@@ -1,6 +1,4 @@
-if not game.PlaceId == 3260590327 or not game.PlaceId == 5591597781 then
-    return
-end
+if game.PlaceId ~= 3260590327 or not game.PlaceId ~= 5591597781 then return end
 
 getgenv().enabled = true
 
